9d3da6f59510e420aea9668eb426d1e3a89eb247
RaBar <rabar.sdiq1995@gmail.com>
2023-06-13 10:01:00 +0300

    init

--- lib/Pages/Report.dart
+++ lib/Pages/Report.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:alpha/Components/Palette.dart';
 import 'dart:typed_data';
+import 'package:alpha/db/connection.dart';
 
 class Reportpage extends StatefulWidget {
   const Reportpage({super.key});
@@ -10,26 +11,111 @@ class Reportpage extends StatefulWidget {
   State<Reportpage> createState() => _ReportpageState();
 }
 
+var conn;
+
 class _ReportpageState extends State<Reportpage> {
+  List<dynamic> results = [];
+
+  getData() async {
+    try {
+      conn = await PostgresConnection.getConnection();
+      // print("connected");
+      results = await conn.mappedResultsQuery('SELECT * FROM user_reg');
+      print("result = " + results.toString());
+
+      setState(() {});
+
+      return results;
+      // ...
+    } catch (e) {
+      // ...
+    } finally {
+      print("closing connection");
+      await conn.close();
+    }
+    // This block will always execute
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Center(
-          child: Text(" پیشاندانی ڕاپۆرت ",style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
-          ),),
-        ),
-        backgroundColor: Palette.alphacolor,
-        elevation: 0,
+    // print("here : " + results.toString());
+
+    return MaterialApp(
+      title: 'Flutter Demo',
+
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: Scaffold(
+        drawer: Drawer(
+          child: Column(
+            children: [
+              ListTile(
+                leading: Icon(Icons.person),
+                title: Text(results[0]['user_reg']['full_name'].toString()),
+              ),
+            ],
+          ),
+        ),
+        appBar: AppBar(
+          title: Text('Flutter Demo'),
+        ),
+        body: ListView.builder(
+          itemCount: results.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              leading: Icon(Icons.person),
+              title: Text(results[index]['user_reg']['full_name'].toString()),
+            );
+          },
+        ),
+      ),
+
     );
+
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     title: Center(
+    //       child: Text(
+    //         " پیشاندانی ڕاپۆرت ",
+    //         style: TextStyle(
+    //           fontSize: 18,
+    //           fontWeight: FontWeight.bold,
+    //           color: Colors.white,
+    //         ),
+    //       ),
+    //     ),
+    //     backgroundColor: Palette.alphacolor,
+    //     elevation: 0,
+    //   ),
+    //   body: FutureBuilder<List<Map<String, dynamic>>>(
+    //     future: getData(),
+    //     builder: (BuildContext context,
+    //         AsyncSnapshot<List<Map<String, dynamic>>> snapshot) {
+    //       if (snapshot.hasData) {
+    //         return ListView.builder(
+    //           itemCount: snapshot.data.length,
+    //           itemBuilder: (BuildContext context, int index) {
+    //             final item = snapshot.data[index];
+    //             return ListTile(
+    //               title: Text(item['title']),
+    //               subtitle: Text(item['subtitle']),
+    //             );
+    //           },
+    //         );
+    //       } else if (snapshot.hasError) {
+    //         return Text('${snapshot.error}');
+    //       }
+
+    //       return const Center(child: CircularProgressIndicator());
+    //     },
+    //   ),
+    // );
   }
 }
-
-
-
-
-
--- lib/main.dart
+++ lib/main.dart
@@ -9,25 +9,18 @@ import 'package:fluent_ui/fluent_ui.dart';
 import 'Components/Palette.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
+void main() => runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/',
 
-void main()=> runApp(MaterialApp(
-  debugShowCheckedModeBanner: false,
-    initialRoute: '/',
-  routes: {
-    'SplashScreen':(context) => LoginForm(),
-    'LoginForm':(context) => Homepage(),
-    'LoginForm':(context) => Thirdpage(),
-    'Thirdpage':(context) => Tomarpage(),
-    'Thirdpage':(context) => Dosyapage(),
-  },
-  home:SplashScreen(),
-));
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
-  }
-}
+        routes: {
+          'SplashScreen': (context) => LoginForm(),
+          'LoginForm': (context) => Homepage(),
+          'LoginForm': (context) => Thirdpage(),
+          'Thirdpage': (context) => Tomarpage(),
+          'Thirdpage': (context) => Dosyapage(),
+        },
+        home: SplashScreen(),
+      ),
+    );
